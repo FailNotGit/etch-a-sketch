e9642af4ef98efb24b47f1a74decc13f5cddd8fc
FailNotGit <156331819+FailNotGit@users.noreply.github.com>
2025-01-17 01:03:54 -0500

    no changes made

--- myScript.js
+++ myScript.js
@@ -5,8 +5,9 @@ const square = document.createElement("div");
 square.classList.add("square");
 square.textContent = " ";
 square.style.borderStyle = "solid";
-square.style.width ="50px";
-square.style.height ="50px";
+square.style.flexGrow = "1"
+const squareHeight = square.style.flexGrow = "1"
+square.style.height = "squareHeight";
 
 square.addEventListener("mouseover", changeColor);
 
@@ -15,18 +16,21 @@ square.addEventListener("mouseover", changeColor);
 
 function createGrid(aSize){ 
     container.style.width = "800px";
-    container.style.height = "800px";
-    square.style.flexGrow = "1"
+    container.style.height = "800px"
+    
+    square.style.nthChild = aSize
 for(let squareCount = 0; squareCount < aSize ; squareCount++){
     const clonedSquare =square.cloneNode(true);
     clonedSquare.addEventListener("mouseover", changeColor);
     container.appendChild(clonedSquare);
-    for(let squareCount = 0; squareCount < aSize; squareCount++){
+    for(let squareCount = 0; squareCount < aSize ; squareCount++){
         const clonedSquare =square.cloneNode(true);
         clonedSquare.addEventListener("mouseover", changeColor);
         container.appendChild(clonedSquare);
+     
     }
 }
+
 }
 
 
@@ -44,4 +48,4 @@ button.classList.add("button");
 button.textContent = "Choose Size";
 body.insertBefore(button, container);
 
-createGrid(10);
+createGrid(20);
