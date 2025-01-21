8e064de916e19efed1afca112d391a226c5052a6
FailNotGit <156331819+FailNotGit@users.noreply.github.com>
2025-01-20 23:52:01 -0500

    created button functionality to create new grid

--- myScript.js
+++ myScript.js
@@ -3,32 +3,42 @@ const container = document.querySelector(".container")
 const body = document.querySelector("body");
 const square = document.createElement("div");
 square.classList.add("square");
-square.textContent = " ";
-square.style.borderStyle = "solid";
-square.style.flexGrow = "1"
-const squareHeight = square.style.flexGrow = "1"
-square.style.height = "squareHeight";
 
-square.addEventListener("mouseover", changeColor);
-
-
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = "800px";
+container.style.height = "800px";
+container.style.margin = "0";
+container.style.padding = "0";
 
 
-function createGrid(aSize){ 
-    container.style.width = "800px";
-    container.style.height = "800px"
-    
-    square.style.nthChild = aSize
-for(let squareCount = 0; squareCount < aSize ; squareCount++){
-    const clonedSquare =square.cloneNode(true);
+square.style.border = "1px solid black";
+square.style.boxSizing = "border-box"; 
+square.style.width = "50px"; 
+square.style.height = "50px"; 
+square.style.paddingBottom = "0";
+
+const button = document.createElement("button");
+
+button.classList.add("button");
+button.textContent = "Choose Size";
+body.insertBefore(button, container);
+button.addEventListener("click", createGrid);
+
+
+
+
+
+function createGrid(){ 
+container.textContent = "";
+let aSize = prompt("Pick a grid size: ");
+const squareSize = container.offsetWidth / aSize; 
+for(let squareCount = 0; squareCount < aSize * aSize; squareCount++){
+    const clonedSquare = square.cloneNode(true);
+    clonedSquare.style.width - `${squareSize}px`;
+    clonedSquare.style.height - `${squareSize}px`;
     clonedSquare.addEventListener("mouseover", changeColor);
     container.appendChild(clonedSquare);
-    for(let squareCount = 0; squareCount < aSize ; squareCount++){
-        const clonedSquare =square.cloneNode(true);
-        clonedSquare.addEventListener("mouseover", changeColor);
-        container.appendChild(clonedSquare);
-     
-    }
 }
 
 }
@@ -39,13 +49,18 @@ function changeColor(event){
     event.target.style.backgroundColor = "black ";
 }
 
-function chooseSize() {
-    
+
+
+function initializeGrid(20) {
+
+    for(let squareCount = 0; squareCount < aSize * aSize; squareCount++){
+        const clonedSquare = square.cloneNode(true);
+        clonedSquare.style.width - `${squareSize}px`;
+        clonedSquare.style.height - `${squareSize}px`;
+        clonedSquare.addEventListener("mouseover", changeColor);
+        container.appendChild(clonedSquare);
+    }
+
 }
-const button = document.createElement("button");
 
-button.classList.add("button");
-button.textContent = "Choose Size";
-body.insertBefore(button, container);
-
-createGrid(20);
+initializeGrid()
